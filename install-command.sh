f91ee50f04572355d4d3bf2b574ca79db5cd953d
vikas <vikaskonda.vicky@gmail.com>
2025-07-06 01:28:23 +0530

    condition statement

--- install-command.sh
+++ install-command.sh
@@ -3,4 +3,6 @@ userid=$(id -u)
 if [ $userid -ne 0]
 then
     echo "Error:: please run this script as root user"
+    exit 1 #exit with error code 1 ,this is used to indicate that the script did not run successfully
 fi #fi is used to end the if statement
+# other than 0 exit code is used to indicate that the script did not run successfully from 1-127 the number may be anything
